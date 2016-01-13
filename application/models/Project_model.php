4f83f3b1a50a9a98dafe4785ca6416ce99349a66
hardac <865391581@qq.com>
2016-01-13 22:25:40 +0800

    增加了分页功能

--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -14,13 +14,24 @@ class Project_model extends CI_Model
     }
     
     public function getLoginInfo($begin,$count,$userID,$subscribeStartDate, $subscribeEndDate, $status){
+        
+    }
+    
+    /* 获取分页数据及总条数
+    * @param string @tablename 表名
+    * @param mixed $where 条件
+    * @param int $limit 每页条数
+    * @param int $offset 当前页
+    */
+    public function getPageData($tablename, $where, $limit, $offset, $order_by, $db)
+    {
         if(empty($tablename))
         {
             return FALSE;
         }
-        
+         
         $dbhandle = empty($db) ? $this->db : $db;
-        
+         
         if($where)
         {
             if(is_array($where))
@@ -32,27 +43,27 @@ class Project_model extends CI_Model
                 $dbhandle->where($where, NULL, false);
             }
         }
-        
+         
         $db = clone($dbhandle);
         $total = $dbhandle->count_all_results($tablename);
-        
+         
         if($limit)
         {
             $db->limit($limit);
         }
-        
+         
         if($offset)
         {
             $db->offset($offset);
         }
-        
+         
         if($order_by)
         {
             $db->order_by($order_by);
         }
-        
+         
         $data = $db->get($tablename)->result_array();
-        
-        return array('total' => $total, 'data' => $data);
+         
+        return $data;
     }
 }
