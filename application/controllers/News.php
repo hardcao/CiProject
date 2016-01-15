454f68a05cd5264f6847b8289627d5b59f435d94
hardac <hardac@163.com>
2016-01-15 14:10:56 +0800

    测试通过第五个接口

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -29,7 +29,12 @@ class News extends CI_Controller
         echo json_encode($result);
         
     }
-    
+    /*
+     * 
+     * 输入：newsId=123
+     *  接口：news/getDynamicNewsDetail
+     * 输出：{"success":true,"errorCode":0,"error":0,"data":[{"FID":"123","FPROJECTID":"123","FTITLE":"\u5408","FCREATORID":"123","FRELEASEDATE":"2014-09-01 09:53:00","FCONTENT":"\u5408\u80a5\u9ad8"}]}
+     * */
     public function  getDynamicNewsDetail() {
         
         $newsId = $this->input->post('newsId');
