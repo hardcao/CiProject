86ee61a9d08e6db4ef031d8ca006118f530ef920
hardac <865391581@qq.com>
2016-01-17 10:02:48 +0800

    增加了图片数据表

---
+++ application/models/Pic_model.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Pic_model extends CI_Model
+{
+    public function __construct()
+    {
+        # code...
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function getFollower($FID) {
+        $this->db->select("*");
+        $this->db->where('FID',$FID);
+        $result = $this->db->get('T_PIC')->result_array();
+        return $result[0];
+    }
+
+    public function  addFollower($dataArry){
+        $insertArry = array(
+            'FPROJECTID'=>$dataArry['FPROJECTID'],
+            'FNAME' =>$dataArry['FNAME'],
+            'FCONTENT' => $dataArry['FCONTENT'],
+            'FISMAINPIC' => $dataArry['FISMAINPIC']
+        );
+        $this->db->insert('T_PIC', $insertArry);
+        $data["success"] = true;
+        $data["errorCode"] = 0;
+        $data["error"] = 0;
+        $data['data'] = '0';
+        return  $data;
+    }
+}
