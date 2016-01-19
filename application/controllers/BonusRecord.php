c4627f2ed3e094f490de0ced7da4470c379042a9
hardac <hardac@163.com>
2016-01-19 14:34:38 +0800

    实现了数据库的跟新

--- application/controllers/BonusRecord.php
+++ application/controllers/BonusRecord.php
@@ -29,7 +29,19 @@ class BonusRecord extends CI_Controller
         $result = $this->BonusRecord_model->addBonusList($data['data']);
     	echo json_encode($result);
     }
-    
+    /*
+     * 
+     * 
+     * 
+     * {"data":[{ "FSUBSCRIBECONFIGRMRECORDID":1,
+			    	"FBONUSTIMES":"12333",
+			    	"FBONUSAMOUNT":"121120",
+			    	"FBONUSDATE":"2014-09-01 09:50:00",
+			    	"FID":1}]}
+     * URL:BonusRecord/updateBonusList
+     * ouput:{"success":true,"errorCode":0,"error":0,"data":true}
+     * 
+     * */
     public function updateBonusList() {
         
         $data = $this->input->input_stream();
