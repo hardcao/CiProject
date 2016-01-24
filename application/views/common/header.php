0607644bafa64000767d28f2e6c3a643cd0d2724
ypandoo <ypandoo@126.com>
2016-01-24 09:35:27 +0800

    add back link

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -7,11 +7,12 @@
                 <li class="n3 more"><a href="<?php echo site_url().'home/index/personalCenter#2';?>"><span>个人中心</span></span><span class="bkg"></span></a></li>
                 <li class="n4 "><a href="<?php echo site_url().'home/index/followRules#3';?>"><span>跟投制度</span><span class="bkg"></span></a></li>
                 <li class="n5 "><a href="<?php echo site_url().'home/index/helpCenter#4';?>"><span>帮助中心</span><span class="bkg"></span></a></li>
+                <li class="n5 "><a href="<?php echo site_url().'back';?>"><span>后台管理</span><span class="bkg"></span></a></li>
             </ul>
 
             <div id="shop">
-                <a class="btn-lang tmp-unselect" href="javascript:void(0);">中/EN</a>
-                <a href="javascript:void(0);" class="btn-search"></a>
+                <a class="btn-lang tmp-unselect" href="javascript:void(0);">登录</a>
+                <!--a href="javascript:void(0);" class="btn-search"></a-->
             </div>
         </div>
     </div>
@@ -20,12 +21,12 @@
         <div id="subNav">
             <div class="g-wrap">
                 <div class="item about">
-                    <a href="<?php echo site_url().'home/index/projectList#1';?>">项目信息<span></a>
+                    <a href="<?php echo site_url().'home/index/projectList?query=1';?>">项目信息<span></a>
                     <a href="<?php echo site_url().'home/index/newsList#1';?>">动态新闻<span></a>
                 </div>
                 <div class="item news">
-                    <a href="<?php echo site_url().'home/index/newsList#2';?>">我要认购<span></a>
-                    <a href="<?php echo site_url().'home/index/newsList#2';?>">未完成认购<span></a>
+                    <a href="<?php echo site_url().'home/index/projectList?query=2';?>">我要认购<span></a>
+                    <a href="<?php echo site_url().'home/index/projectList?query=3';?>">未完成认购<span></a>
                     <a href="<?php echo site_url().'home/index/completed#2';?>">已完成认购<span></a>
                     <a href="<?php echo site_url().'home/index/payInDetail#2';?>">缴款确认<span></a>
                     <a href="<?php echo site_url().'home/index/bonusDetail#2';?>">分红明细<span></a>
@@ -34,11 +35,11 @@
                 <div class="item search">
                     <input id='searchbox' type="text" placeholder="请输入关键词" />
                 </div>
-                <div id="language-box" class="item langs">
+                <!--div id="language-box" class="item langs">
                     <a id="on" href="../">中文简体</a>
                     <a href="/big5">中文繁体</a>
                     <a href="/en">EN</a>
-                </div>
+                </div-->
             </div>
         </div>
         <div style="height:67px; width:100%">
