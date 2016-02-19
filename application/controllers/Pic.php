8476aefb8c0c77062ce1c9799b0fe2955be6d3d5
ypandoo <ypandoo@126.com>
2016-02-19 16:54:17 +0800

    upload image

--- application/controllers/Pic.php
+++ application/controllers/Pic.php
@@ -84,7 +84,7 @@ class Pic extends CI_Controller
         $ex = explode(",",$this->input->post('pic_data'));//分割data-url数据
         $filter=explode('/', trim($ex[0],';base64'));//获取文件类型
         $ss = base64_decode(str_replace($filter[1] , '', $ex[1]));//图片解码
-        $picname =  md5( 'images/'.$id.'.'.$filter[1]);//生成文件名
+        $picname =  './images/'.md5(uniqid(rand())).'.'.$filter[1];//生成文件名
         $this->base64_to_jpeg($this->input->post('pic_data'), $picname);
         $insertdata['FCONTENT'] = $picname;
         $tableName = 'T_PIC';
