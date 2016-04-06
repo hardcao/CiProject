752b405e49686bc2886f0e5d8c354b626d3a88a9
ypandoo <ypandoo@126.com>
2016-04-06 11:18:50 +0800

    aa

--- application/views/common/js/public.js
+++ application/views/common/js/public.js
@@ -57,6 +57,7 @@ $(function () {
         index_hash = getHash();
         //index_hash = parseInt(index_hash.substring(1,index_hash.Length));
         link_page = links.eq(index_hash)[0];
+        if (typeof(link_page) == "undefined" || link_page == null) {window.location.reload();}
         control(nav_on = link_curr = link_page, false);
 
         if (index_hash != 0)
