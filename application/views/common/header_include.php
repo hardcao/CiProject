c8d81f71c97999af080078960f848b5d20d4f4f5
ypandoo <ypandoo@126.com>
2016-04-06 14:15:45 +0800

    aa

--- application/views/common/header_include.php
+++ application/views/common/header_include.php
@@ -24,4 +24,4 @@
 <link rel="stylesheet"  type="text/css" href="<?php echo site_url('application/views/common/css/index.css')?>" />
 <link rel="stylesheet"  type="text/css" href="<?php echo site_url('application/views/common/css/font-dincond.css')?>" />
 <script type="text/javascript" src="<?php echo site_url('application/views/plugins/jquery-1.8.0.min.js')?>"></script>
-<script type="text/javascript" src="<?php echo site_url('application/views/common/js/public.js')?>"></script>
+<script type="text/javascript" defer="defer" src="<?php echo site_url('application/views/common/js/public.js')?>"></script>
